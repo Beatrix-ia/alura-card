546ca144c6ced429791ec6b3824d4056f20504ba
Beatrix-ia <00001118453189sp@al.educacao.sp.gov.br>
2024-11-04 12:23:36 -0300

    .

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'animal',
+    'O que é um cachorro?'
+    'cachorro é um animal que late'
+)
+
+criaCartao(
+    'brinquuedo',
+    'o que é uma boneca?',
+    'é um brinquedon de menina que parece um ser humano, tem boca,cabelo, corpo etc'
+m)
+
+criaCartao(
+    'hora do banho',
+    'O que é um banho?',
+    'onde vc lava seu cabelo, lava seu corpo, faz depilação'
+)
+
+criaCartao(
+    'sobre sentimento'
+    'o que é amar?',
+    'é um sentimento, que demontra um carimho enorme pela pessoa'
+)
